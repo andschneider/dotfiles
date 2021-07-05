562507f3b5ef6e1e8cc1d60bea4dfd21e183bf3d
Andrew Schneider <andjschneider@gmail.com>
2021-07-05 15:11:40 -0700

    Reformat with lua-formatter

--- nvim/lua/config/lspconfig.lua
+++ nvim/lua/config/lspconfig.lua
@@ -1,32 +1,21 @@
-local nvim_lsp = require'lspconfig'
+local nvim_lsp = require 'lspconfig'
 
 -- Rust 
 -- based on: https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
 -- https://sharksforarms.dev/posts/neovim-rust/
 nvim_lsp.rust_analyzer.setup({
-    on_attach=on_attach,
+    on_attach = on_attach,
     settings = {
         ["rust-analyzer"] = {
-            assist = {
-                importGranularity = "module",
-                importPrefix = "by_self",
-            },
-            cargo = {
-                loadOutDirsFromCheck = true
-            },
-            procMacro = {
-                enable = true
-            },
+            assist = {importGranularity = "module", importPrefix = "by_self"},
+            cargo = {loadOutDirsFromCheck = true},
+            procMacro = {enable = true}
         }
     }
 })
 
 -- Enable diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-  }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
+                 {virtual_text = true, signs = true, update_in_insert = true})
 
