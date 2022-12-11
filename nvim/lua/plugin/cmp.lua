2a0fae4b8b34edc821c338274c78983a285ff41b
Andrew Schneider <andjschneider@gmail.com>
2022-12-11 12:48:05 -0800

    aliases: Add cargo test --exact shortcut
    alias for showing test output using `cargo test -- --show-output --exact`:
    
    * ctse <test name>

--- nvim/lua/plugin/cmp.lua
+++ nvim/lua/plugin/cmp.lua
@@ -43,5 +43,5 @@ cmp.setup {
 }
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require("lspconfig")["rust_analyzer"].setup { capabilities = capabilities }
